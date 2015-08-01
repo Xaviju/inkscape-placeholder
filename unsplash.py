174dfc09e61ca5641d2a2f9f3139edb8a1425ef5
David Barragán Merino <david.barragan@kaleidos.net>
2015-08-01 13:54:06 +0200

    It's alive!! :dancers:

--- unsplash.py
+++ unsplash.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import base64
+import sys
 import urllib2
 import inkex
 from lxml import etree
 
-class unsplashPlaceholder(inkex.Effect):
+
+class UnsplashPlaceholder(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
-        #self.OptionParser.add_option('-w', '--width', action='store', type='string', dest='width', default='640', help='Set image width')
-        #self.OptionParser.add_option('-h', '--height', action='store', type='string', dest='height', default='480', help='Set image height')
+        self.OptionParser.add_option('-W', '--width', action='store', type='string', dest='width',
+                                     default='800', help='Set image width')
+        self.OptionParser.add_option('-H', '--height', action='store', type='string', dest='height',
+                                     default='680', help='Set image height')
 
-    def getImage(self):
-        #width = self.options.width
-        width = '800'
-        #height = self.options.height
-        height = '680'
+    def effect(self):
+        image = self._get_image()
+        node = self._create_image_node(image)
 
-        #url = 'https://unsplash.it/' + self.options.width + '/' + self.options.height + '/?random'
-        url = 'https://unsplash.it/' + width + '/' + height + '/?random'
+        self.document.getroot().append(node)
+
+    def _get_image(self):
+        url = 'https://unsplash.it/{width}/{height}/?random'.format(width=self.options.width,
+                                                                    height=self.options.height)
         response = urllib2.urlopen(url)
         data = response.read()
-        print data
         return data
 
-    def createImage(self, data):
-        svg = etree.Element("svg")
-        #Create a new layer.
-        layer = inkex.etree.SubElement(svg, 'g')
-        layer.set(inkex.addNS('label', 'inkscape'), 'unsplash')
-        layer.set(inkex.addNS('groupmode', 'inkscape'), 'layer')
-
+    def _create_image_node(self, data):
         attribs = {
-            'height'    : '800',
-            'width'     : '600',
-            'x'         : '0',
-            'y'         : '0',
+            'height': self.options.height,
+            'width': self.options.width,
+            'x': '0',
+            'y': '0',
             'preserveAspectRatio': 'None',
-            inkex.addNS('xlink','href'): 'data:image/jpeg;base64,' + data
+            inkex.addNS('href', 'xlink'): u'data:image/jpeg;base64,' + base64.encodestring(data)
         }
-        image = inkex.etree.Element(inkex.addNS('image','svg'), attribs)
-        layer.append(image)
+        node = inkex.etree.Element(inkex.addNS('image','svg'), attribs)
+        return node
 
-placeholder = unsplashPlaceholder()
-getImage = placeholder.getImage()
-placeholder.createImage(getImage)
+
+if __name__ == '__main__':
+    placeholder = UnsplashPlaceholder()
+    placeholder.affect()
